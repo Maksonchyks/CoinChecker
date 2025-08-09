a7b51df7943080814798e4e1719473ee65776733
Maksonchyks <musinmaksim28@email.com>
2025-08-09 12:08:04 +0300

    Added field currentView for navigation

--- CoinChecker/ViewModels/MainViewModel.cs
+++ CoinChecker/ViewModels/MainViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoinChecker.Core;
 
 namespace CoinChecker.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private ViewModelBase _currentView;
+
+        public ViewModelBase CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
