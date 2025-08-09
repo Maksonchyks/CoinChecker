8d9b6325ea631e66fbdbd5822edfcad75b270177
Maksonchyks <musinmaksim28@email.com>
2025-08-09 14:37:35 +0300

    register new services and onStartup

--- CoinChecker/App.xaml.cs
+++ CoinChecker/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using CoinChecker.Services.Interfaces;
+using CoinChecker.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+using CoinChecker.ViewModels;
 
 namespace CoinChecker;
 
@@ -9,5 +13,31 @@ namespace CoinChecker;
 /// </summary>
 public partial class App : Application
 {
+    private readonly IServiceProvider _serviceProvider;
+    public App()
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<INavigationService, NavigationService>();
+
+        services.AddSingleton<MainViewModel>();
+
+        services.AddTransient<HomeViewModel>();
+        services.AddTransient<DetailsViewModel>();
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainViewModel = _serviceProvider.GetService<MainViewModel>();
+
+        var mainWindow = new MainWindow();
+
+        mainWindow.DataContext = mainViewModel;
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
 }
 
