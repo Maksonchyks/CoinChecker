f4e7ff4a132f3c566e701a9e5793c9a1f820ea7c
Maksonchyks <musinmaksim28@email.com>
2025-08-09 14:57:15 +0300

    Added implementation for NavigationService

---
+++ CoinChecker/Services/Implementations/NavigationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinChecker.Core;
+using CoinChecker.Services.Interfaces;
+using CoinChecker.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoinChecker.Services.Implementations
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly MainViewModel _mainViewModel;
+        public NavigationService(MainViewModel mainViewModel, IServiceProvider serviceProvider) 
+        {
+            _serviceProvider = serviceProvider;
+            _mainViewModel = mainViewModel;
+        }
+        public void NavigateTo<TPage>() where TPage : ViewModelBase
+        {
+            var newViewModel = _serviceProvider.GetService<TPage>();
+            _mainViewModel.CurrentView = newViewModel;
+        }
+    }
+}
---
+++ CoinChecker/Services/Interfaces/INavigationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinChecker.Core;
+
+namespace CoinChecker.Services.Interfaces
+{
+    public interface INavigationService
+    {
+        void NavigateTo<TPage>() where TPage : ViewModelBase;
+    }
+}
