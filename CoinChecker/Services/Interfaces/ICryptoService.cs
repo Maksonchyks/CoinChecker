0d1068afce7dc86fe3c0568a72f7b33a9c81c5ff
Maksonchyks <musinmaksim28@email.com>
2025-08-16 22:34:52 +0300

    Extended crypto service

--- CoinChecker/Services/Interfaces/ICryptoService.cs
+++ CoinChecker/Services/Interfaces/ICryptoService.cs
@@ -10,7 +10,7 @@ namespace CoinChecker.Services.Interfaces
     public interface ICryptoService
     {
         Task<List<CryptoCurrency>> GetTopCryptoAsync(int limit = 10);
-        Task<List<CryptoDetails>> GetCryptoDetailsAsync(string slug);
+        Task<CryptoDetails> GetCryptoDetailsAsync(string slug);
         Task<List<CryptoMarket>> GetTopCurrencyByMarketAsync(string exchangeId);
         Task<List<CryptoMarket>> GetTopMarketByCurrencyAsync(string slug);
         Task<List<MarketPlace>> GetallMarketPlacesAsync(int limit = 10);
