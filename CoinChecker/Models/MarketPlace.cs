83fc10275736dbe75ce0b8ff9c520d7cc68f8c88
Maksonchyks <musinmaksim28@email.com>
2025-08-16 22:32:12 +0300

    Added one more entety

---
+++ CoinChecker/Models/MarketPlace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinChecker.Models
+{
+    public class MarketPlace
+    {
+        public string exchangeId { get; set; }
+        public string exchangeUrl { get; set; }
+    }
+}
